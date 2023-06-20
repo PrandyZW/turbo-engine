88f41d58b4aa3c746db10a7a184a99eba7baea58
PrandyZW <1322850251@qq.com>
2023-06-20 10:17:31 +0800

    初始化项目

---
+++ demo/src/main/java/com/didiglobal/turbo/demo/controller/WorkController.java
@@ -0,0 +1,14 @@
+package com.didiglobal.turbo.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/work")
+public class WorkController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowController.class);
+
+
+}
